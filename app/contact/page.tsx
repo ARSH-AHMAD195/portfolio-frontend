107bb29d6984c4d03142ebe59baf7249fa74ced3
Arsh Ahmad <165986419+ARSH-AHMAD195@users.noreply.github.com>
2026-03-26 14:32:05 +0530

    Add Contact and Footer components to ContactPage

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,6 +4,8 @@ function ContactPage() {
   return (
     <main className="w-full flex flex-col justify-between items-center overflow-x-hidden">
       <Navbar />
+      <Contact />
+      <Footer />
     </main>
   )
 }
